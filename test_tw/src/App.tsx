a810bb759157960ff9edb9fe3b779a5229746dc7
Alina <alinbrode@yandex.ru>
2025-06-15 16:56:46 +0300

    added test_tw

---
+++ test_tw/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="bg-red-500 text-white p-4">TEST</div>
+      </div>
+    </>
+  )
+}
+
+export default App
