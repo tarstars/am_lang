c0d7d53dd81addeb45abf24e95d3bf1a7b2f786c
Alina <alinbrode@yandex.ru>
2025-06-15 19:55:37 +0300

    tailwind started to work

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,9 @@ export default function App() {
     <LanguageProvider>
       <BrowserRouter>
         <div className="flex min-h-screen">
+        <div className="min-h-screen flex items-center justify-center">
+          <div className="bg-red-500 text-white p-4">TEST</div>
+        </div>
           <SideNav />
           <div className="flex-1 ml-64">
             <Routes>
